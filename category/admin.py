42d53d47da36f5eca47f5ee7041d4fb4211ff97f
Rebeccanak <rebeccanakachwaaa@gmail.com>
2023-06-19 19:33:31 +0300

    commit changes

--- category/admin.py
+++ category/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Customer
+from .models import Category
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display =("productName","productSize")
 
-admin.site.register(Customer,CategoryAdmin) 
+admin.site.register(Category,CategoryAdmin) 
--- greenkiosk/settings.py
+++ greenkiosk/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'inventories' ,
-    'customer'
+    'customer',
+    'category'
 
 ]
 
